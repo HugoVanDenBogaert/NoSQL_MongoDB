8881516a3a049b1324e17bd4bf7d75cacd8715f8
Hugo <vandenbogaert.hugo@gmail.com>
2024-09-19 12:32:38 +0200

    Modelisation des données
    Ecriture des commande pour mettre en place la structure des données.

--- commands.sh
+++ commands.sh
@@ -1,1 +1,78 @@
 # écrire ici les commandes pour respecter les consignes écrites dans le README ou la diapo
+
+# insertion d'une equipe
+db.equipe.insertOne(
+    {
+        nom: "mont st-michel",
+        couleur: "gris",
+        stade: "mi-shell",
+        effectif: [ {
+            nom: "michel",
+            prenom: "jean",
+            dob: "03 janvier 1956",
+            taille: 1.60,
+            poids: 70,
+            poste: "patissier"
+        } ]
+    }
+);
+
+# insertion d'un joueur
+db.joueur.insertOne(
+    {
+        _id: "jghezfifoisdfsfj",
+        nom: "michel",
+        prenom: "jean",
+        dob: "03 janvier 1956",
+        taille: 1.60,
+        poids: 70,
+        poste: "patissier"
+    }
+);
+
+#insertion d'un match
+db.match.insertOne(
+    {
+        domicile: 
+    {
+        nom: "mont st-michel",
+        couleur: "gris",
+        stade: "mi-shell",
+        effectif: [ {
+            _id: "jghezfifoisdfsfj",
+            nom: "michel",
+            prenom: "jean",
+            dob: "03 janvier 1956",
+            taille: 1.60,
+            poids: 70,
+            poste: "patissier"
+        } ]
+    },
+        exterieure: 
+    {
+        nom: "mont st-michel",
+        couleur: "gris",
+        stade: "mi-shell",
+        effectif: [ {
+            _id: "jghezfifoisdfsfj",
+            nom: "michel",
+            prenom: "jean",
+            dob: "03 janvier 1956",
+            taille: 1.60,
+            poids: 70,
+            poste: "patissier"
+        } ]
+    },
+        competition: "championnat regionnal de bretagne",
+        scoreDomicile: 0,
+        scoreExterieure: 6,
+        noteDomicile: [ {
+            _id: "jghezfifoisdfsfj",
+            note: 3
+        }],
+        noteExterieure: [{
+            _id: "jghezfifoisdfsfj",
+            note: 3
+        }]
+    }
+)
