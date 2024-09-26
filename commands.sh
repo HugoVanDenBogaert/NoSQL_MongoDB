95a45289482714704bda6a9be24b3f648ab9c773
vorms.clement <clement.vorins@gmail.com>
2024-09-26 14:08:33 +0200

    Update commands.sh
    tkt samarsh bientot

--- commands.sh
+++ commands.sh
@@ -1,78 +1,175 @@
-# écrire ici les commandes pour respecter les consignes écrites dans le README ou la diapo
 
-# insertion d'une equipe
-db.equipe.insertOne(
+// Insertion de joueurs
+db.joueur.insertMany([
     {
-        nom: "mont st-michel",
-        couleur: "gris",
-        stade: "mi-shell",
-        effectif: [ {
-            nom: "michel",
-            prenom: "jean",
-            dob: "03 janvier 1956",
-            taille: 1.60,
-            poids: 70,
-            poste: "patissier"
-        } ]
-    }
-);
-
-# insertion d'un joueur
-db.joueur.insertOne(
-    {
-        _id: "jghezfifoisdfsfj",
-        nom: "michel",
-        prenom: "jean",
-        dob: "03 janvier 1956",
+        nom: "Michel",
+        prenom: "Jean",
+        dob: new Date("1956-01-03"),
         taille: 1.60,
         poids: 70,
         poste: "patissier"
+    },
+    {
+        nom: "Debrac",
+        prenom: "Pierre",
+        dob: new Date("1998-06-15"),
+        taille: 1.75,
+        poids: 80,
+        poste: "arrière droit"
+    },
+    {
+        nom: "Lefevre",
+        prenom: "Jacques",
+        dob: new Date("1992-11-15"),
+        taille: 1.78,
+        poids: 75,
+        poste: "milieu"
+    },
+    {
+        nom: "Martin",
+        prenom: "Louis",
+        dob: new Date("1995-02-25"),
+        taille: 1.90,
+        poids: 85,
+        poste: "attaquant"
     }
-);
+]);
 
-#insertion d'un match
-db.match.insertOne(
+
+# insertion d'une equipe
+db.equipe.insertMany([
     {
-        domicile: 
-    {
-        nom: "mont st-michel",
+        nom: "mont st-michel angels",
         couleur: "gris",
         stade: "mi-shell",
-        effectif: [ {
-            _id: "jghezfifoisdfsfj",
-            nom: "michel",
-            prenom: "jean",
-            dob: "03 janvier 1956",
-            taille: 1.60,
-            poids: 70,
-            poste: "patissier"
-        } ]
+        effectif: [
+		    {
+        nom: "Michel",
+        prenom: "Jean",
+        dob: new Date("1956-01-03"),
+        taille: 1.60,
+        poids: 70,
+        poste: "patissier"
     },
-        exterieure: 
     {
-        nom: "mont st-michel",
-        couleur: "gris",
-        stade: "mi-shell",
-        effectif: [ {
-            _id: "jghezfifoisdfsfj",
-            nom: "michel",
-            prenom: "jean",
-            dob: "03 janvier 1956",
-            taille: 1.60,
-            poids: 70,
-            poste: "patissier"
-        } ]
+        nom: "Debrac",
+        prenom: "Pierre",
+        dob: new Date("1998-06-15"),
+        taille: 1.75,
+        poids: 80,
+        poste: "arrière droit"
+    }]
     },
-        competition: "championnat regionnal de bretagne",
-        scoreDomicile: 0,
-        scoreExterieure: 6,
-        noteDomicile: [ {
-            _id: "jghezfifoisdfsfj",
-            note: 3
-        }],
-        noteExterieure: [{
-            _id: "jghezfifoisdfsfj",
-            note: 3
-        }]
+    {
+        nom: "st-malo pirates",
+        couleur: "bleu",
+        stade: "corsair",
+        effectif: [
+		    {
+        nom: "Lefevre",
+        prenom: "Jacques",
+        dob: new Date("1992-11-15"),
+        taille: 1.78,
+        poids: 75,
+        poste: "milieu"
+    },
+    {
+        nom: "Martin",
+        prenom: "Louis",
+        dob: new Date("1995-02-25"),
+        taille: 1.90,
+        poids: 85,
+        poste: "attaquant"
+    }]
     }
-)
+]);
+
+// Insertion de matchs
+db.match.insertMany([
+    {
+        domicile: "Mont St-Michel",
+        exterieure: "St-Malo Pirates",
+        competition: "Championnat Breizh vs Normandie",
+        scoreDomicile: 2,
+        scoreExterieure: 3,
+        notesDomicile: [
+            { nom: "Michel", prenom: "Jean", note: 7 },
+            { nom: "Debrac", prenom: "Pierre", note: 6 }
+        ],
+        notesExterieure: [
+            { nom: "Lefevre", prenom: "Jacques", note: 8 },
+            { nom: "Martin", prenom: "Louis", note: 9 }
+        ]
+    },
+    {
+        domicile: "St-Malo Pirates", # match retour
+        exterieure: "Mont St-Michel",
+        competition: "Championnat Breizh vs Normandie",
+        scoreDomicile: 1,
+        scoreExterieure: 1,
+        notesDomicile: [
+            { nom: "Lefevre", prenom: "Jacques", note: 6 },
+            { nom: "Martin", prenom: "Louis", note: 5 }
+        ],
+        notesExterieure: [
+            { nom: "Michel", prenom: "Jean", note: 7 },
+            { nom: "Debrac", prenom: "Pierre", note: 6 }
+        ]
+    }
+]);
+
+
+db.equipe.createIndex({ nom: 1 });
+# On crée un index simplement par nom parce que le nombre d'homonymes sera faible
+db.joueur.createIndex({ nom: 1 });
+
+maxDob = new Date();
+maxDob.setFullYear(TodayDate.getFullYear() - 35); 
+
+db.joueur.find({
+  	$or: [{poste: "arrière droit"}, {poste:"milieu"}],
+    dob: { $gte: maxDob } 
+});
+
+
+// Aggregation -> joueur + notes 
+db.match.aggregate([
+    {
+        $project: {
+            notesDomicile: 1,
+            notesExterieure: 1
+        }
+    },
+    {
+        $unwind: "$notesDomicile",
+    },
+	{
+		$unwind: "$notesExterieure"
+	},
+    {
+        $group: {
+            _id: {
+                nom: "$notesDomicile.nom",
+                prenom: "$notesDomicile.prenom"
+            },
+            totalNotesDom: { $sum:"$notesDomicile.note"},
+						totalNotesExt: {$sum : "$notesExterieure.note"},
+            matchCount: { $sum: 1 },
+        }
+    },
+    {
+        $match: {
+            matchCount: { $gte: 3 } // Filtrer pour les joueurs ayant joué au moins 3 matchs
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            nom: "$_id.nom",
+            prenom: "$_id.prenom",
+            moyenneNotes: { $divide: [{$add:["$totalNotesDom", "$totalNotesExt"]}, "$matchCount"] } // Calculer la moyenne des notes
+        }
+    }
+]);
+
+
